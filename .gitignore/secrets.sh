ac2e784c2e3cd19fc12e3f210d7c7e85321f19ab
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-11 16:07:33 -0700

    Initial commit

---
+++ .gitignore/secrets.sh
@@ -0,0 +1,8 @@
+# Secrets for your Twitter app
+#
+# This file should never go into github!
+
+export TWITTER_CONSUMER_KEY="fill-this-in"
+export TWITTER_CONSUMER_SECRET="fill-this-in"
+export TWITTER_ACCESS_TOKEN_KEY="fill-this-in"
+export TWITTER_ACCESS_TOKEN_SECRET="fill-this-in"
